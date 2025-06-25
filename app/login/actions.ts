28d8f7b452364001364067ffa8ec0406f8f46d37
SanghyunLee <lgw9736@naver.com>
2025-06-25 20:17:02 +0900

    chore: 로그인 로직 수정

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,8 +1,6 @@
-// loginAction.ts 수정
 'use server';
 
 import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
 
 export async function loginAction({ userId, password }: { userId: string; password: string }) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
@@ -12,19 +10,10 @@ export async function loginAction({ userId, password }: { userId: string; passwo
     credentials: 'include',
   });
 
-  const data = await res.json();
-
   if (!res.ok) {
+    const data = await res.json();
     throw new Error(data.message || '로그인 실패');
   }
-
-  (await cookies()).set('accessToken', data.accessToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60,
-    sameSite: 'strict',
-    path: '/',
-  });
 
   redirect('/dashboard');
 }
